3470b7f7f741bea9e431d73a3e38be44e276bf25
RKPH <pnghung2003@gmail.com>
2024-03-01 20:11:31 +0700

    trang chính của website engforum

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,39 +1,48 @@
 import React from "react";
+import SearchIcon from "@mui/icons-material/Search";
+import LogoIcon from "../../assests/Image 109.png";
+
 
 const Header = () => {
   return (
-    <header className="h-20 w-full bg-red-500 border-b flex items-center justify-center">
+    <header className="fixed top-0 left-0 right-0 h-20 w-full bg-white border-b flex items-center justify-between" style={{zIndex:'99999'}}>
       {/* Logo */}
-      <div className="absolute left-4">
-         logo
+      <div className="flex flex-row justify-center items-center w-[15%] h-full">
+        <img src={LogoIcon} alt="Logo" className="h-12 w-22 mr-2" />
+        <div className="font-bold text-3xl">ITEC</div>
       </div>
 
       {/* Navbar */}
-      <nav className="w-[70%] h-full flex bg-[F3F4F6] items-center">
+      <nav className="w-[60%] h-full flex bg-[F3F4F6] items-center">
         <form className="relative">
           <input
             type="text"
-            className="w-[499px] h-[51px] pl-12 pr-14 py-3 rounded-3xl bg-gray-100 border border-transparent focus:outline-none focus:border-black"
+            className="w-[599px] h-[51px] pl-12 pr-14 py-3 ml-6 rounded-3xl bg-gray-100 border border-transparent focus:outline-none focus:border-black"
             placeholder="Search..."
           />
           <button
             type="submit"
             className="absolute right-0 top-0 h-full px-3 py-3"
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6 text-gray-400 hover:text-gray-700 cursor-pointer"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-            >
-              <path
-                fillRule="evenodd"
-                d="M9 0a9 9 0 0 0-7.214 14.583L0 18l3.417-1.786A9 9 0 1 0 9 0zm0 16a7 7 0 1 1 0-14 7 7 0 0 1 0 14z"
-              />
-            </svg>
+            <SearchIcon className="h-6 w-6 text-gray-400 hover:text-gray-700 cursor-pointer" />
           </button>
         </form>
       </nav>
+
+      <div className="flex items-center justify-end w-[25%] h-full px-3">
+        <button
+          className="flex-grow-0 flex-shrink-0 bg-black text-white w-32 h-[42px] border border-zinc-900 px-4 py-2 rounded-[18px] mr-4 transition duration-300 ease-in-out hover:bg-gray-800 hover:border-gray-800"
+          style={{ lineHeight: "1" }}
+        >
+          Login
+        </button>
+        <button
+          className="flex-grow-0 flex-shrink-0 bg-white w-32 h-[42px] border border-black px-4 py-2 rounded-[18px] text-zinc-900 transition duration-300 ease-in-out hover:bg-gray-200 hover:border-gray-800"
+          style={{ lineHeight: "1" }}
+        >
+          Sign Up
+        </button>
+      </div>
     </header>
   );
 };

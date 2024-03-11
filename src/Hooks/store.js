431339d3df53563f5dcef9c5fd6a1e250856a08a
RKPH <pnghung2003@gmail.com>
2024-03-11 08:55:24 +0700

    # 10 upload post

--- src/Hooks/store.js
+++ src/Hooks/store.js
@@ -1,8 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import postReducer from "./postslice";
+import userReducer from  "./UserSlice";
+import authReducer from "./authSlice";
 
 export default configureStore({
   reducer: {
     post: postReducer,
+    user: userReducer,
+    auth: authReducer, // Include authReducer for authentication state
   },
 });
